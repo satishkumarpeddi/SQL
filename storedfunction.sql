bb3b0a7b4b842779a29894d1f72b776df4ba19a3
evhook <anonymous1986222@gmail.com>
2026-04-21 22:23:14 +0530

    Refactor stored function to improve clarity and organization, including commented-out code for salary calculation and enhanced addition function.

--- storedfunction.sql
+++ storedfunction.sql
@@ -1,18 +1,49 @@
-create or replace function get_annual_salary(
-    p_salary number
-)
-return number
-is 
-    v_annual number;
+-- create or replace function get_annual_salary(
+--     p_salary number
+-- )
+-- return number
+-- is 
+--     v_annual number;
+-- begin
+--    v_annual := p_salary*12;
+--    return v_annual;
+-- end;
+-- /
+-- DECLARE
+--     v_result number;
+-- begin
+--   v_result := get_annual_salary(100000);
+--   DBMS_OUTPUT.PUT_LINE(v_result);
+-- end;
+-- /
+
+-- create or replace function get_annual_salary(p_salary number) return number is v_annual number;
+-- begin
+--  v_annual := p_salary * 12;
+--  return v_annual; 
+-- end;
+-- /
+-- DECLARE
+--     v_num_annual number := 0;
+-- begin
+--     v_num_annual:= get_annual_salary(100000);
+--     dbms_output.put_line(v_num_annual);
+-- end;
+-- /
+
+--sorted functions for addition of two number
+
+
+create or replace function add_method_function(a number,b number) return number is result number;
 begin
-   v_annual := p_salary*12;
-   return v_annual;
+ result:=a+b;
+ return result; 
 end;
 /
-DECLARE
-    v_result number;
+declare
+    result_value number := 0;
 begin
-  v_result := get_annual_salary(100000);
-  DBMS_OUTPUT.PUT_LINE(v_result);
+    result_value := add_method_function(10,30);
+    dbms_output.put_line('Result value := '||result_value);
 end;
 /
